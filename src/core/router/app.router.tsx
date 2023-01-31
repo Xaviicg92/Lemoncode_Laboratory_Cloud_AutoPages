cab78d21d11817098c397e9bfba471febb2a15ae
Javier Cambón <cg_javi@hotmail.com>
2023-01-31 16:45:02 +0100

    changes Laboratory 7

--- src/core/router/app.router.tsx
+++ src/core/router/app.router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { AnimalsContextProvider } from "@/pods/context/context.component";
 
 import { KittiesScene } from "@/scenes";
@@ -9,12 +9,12 @@ import { switchRoutes } from "./routes";
 export const AppRouter = () => {
   return (
     <AnimalsContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={switchRoutes.kitties} element={<KittiesScene />}></Route>
           <Route path={switchRoutes.puppies} element={<PuppiesScene />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AnimalsContextProvider>
   );
 };
